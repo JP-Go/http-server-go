8d90050dfe487b96526a91be15bcaad71cca6e18
João Pedro <jpedro1580@gmail.com>
2024-12-04 00:06:57 -0300

    feat(server): create file server

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("GET /", fileServer)
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
