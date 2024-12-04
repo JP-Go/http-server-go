160b214f09c7d8f094fa53b2f8c58ed5d5711f5f
João Pedro <jpedro1580@gmail.com>
2024-12-04 00:25:05 -0300

    feat(server): add healthz endpoint

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("GET /", fileServer)
+	fileServer := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("GET /app/", fileServer)
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
