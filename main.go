b703e3810e90a67e22284d28d4259afd60ea4dc8
João Pedro <jpedro1580@gmail.com>
2024-12-08 16:24:54 -0300

    refactor(main): move related code to it's own files

--- main.go
+++ main.go
@@ -1,28 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"sync/atomic"
 )
-
-type apiConfig struct {
-	fileServerHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileServerHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("content-type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	cfg.fileServerHits.Store(0)
-	w.Write([]byte("OK"))
-}
 
 func main() {
 	cfg := apiConfig{
@@ -31,18 +12,14 @@ func main() {
 	mux := http.NewServeMux()
 	fileServer := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	mux.Handle("GET /app/", cfg.middlewareMetricsInc(fileServer))
-	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("content-type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		hitsMsg := fmt.Sprintf("Hits: %d", cfg.fileServerHits.Load())
-		w.Write([]byte(hitsMsg))
+
+	mux.HandleFunc("GET /api/healthz/", handleReadiness)
+	mux.HandleFunc("GET /api/metrics/", cfg.handleMetrics)
+	mux.HandleFunc("POST /api/reset/", cfg.resetMetrics)
+	mux.HandleFunc("POST /api/reset", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/api/reset/", http.StatusPermanentRedirect)
 	})
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("content-type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
-	mux.HandleFunc("POST /reset", cfg.resetMetrics)
+
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
---
+++ readiness.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func handleReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
