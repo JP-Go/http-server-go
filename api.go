222834e068c09badf571cbcbab7c79b9741f25e8
João Pedro <jpedro1580@gmail.com>
2024-12-10 23:13:06 -0300

    refactor(metrics): make admin the prefix

--- api.go
+++ api.go
@@ -25,7 +25,15 @@ func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("content-type", "text/plain; charset=utf-8")
+	w.Header().Add("content-type", "text/html; charset=utf-8")
+	w.Header().Add("cache-control", "no-store")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits.Load())))
+	w.Write([]byte(fmt.Sprintf(`
+    <html>
+        <body>
+            <h1>Welcome, Chirpy Admin</h1>
+            <p>Chirpy has been visited %d times!</p>
+        </body>
+    </html>
+    `, cfg.fileServerHits.Load())))
 }
--- main.go
+++ main.go
@@ -11,19 +11,16 @@ func main() {
 	}
 	mux := http.NewServeMux()
 	fileServer := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
-	mux.Handle("GET /app/", cfg.middlewareMetricsInc(fileServer))
+	mux.Handle("GET /app", cfg.middlewareMetricsInc(fileServer))
 
-	mux.HandleFunc("GET /api/healthz/", handleReadiness)
-	mux.HandleFunc("GET /api/metrics/", cfg.handleMetrics)
-	mux.HandleFunc("POST /api/reset/", cfg.resetMetrics)
-	mux.HandleFunc("POST /api/reset", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/api/reset/", http.StatusPermanentRedirect)
-	})
+	mux.HandleFunc("GET /admin/metrics", cfg.handleMetrics)
+	mux.HandleFunc("POST /admin/reset", cfg.resetMetrics)
+
+	mux.HandleFunc("GET /api/healthz", handleReadiness)
 
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
 	server.ListenAndServe()
-
 }
